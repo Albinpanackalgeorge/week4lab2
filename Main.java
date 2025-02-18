548327c001172eae3ad06ed630afaf55d14e7db1
Albinpanackalgeorge <pgeorgealbin@gmail.com>
2025-02-17 23:07:48 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,6 @@ public class Main {
             Movie movie1 = new Movie("Inception", "Christopher Nolan", 2010, 8.8);
             System.out.println(movie1);
 
-            // Uncomment to test validation errors
-            // Movie movie2 = new Movie("", "Nolan", 2025, 9.0); // Invalid title
-            // Movie movie3 = new Movie("Interstellar", "Nolan", 1800, 9.0); // Invalid year
-            // Movie movie4 = new Movie("Tenet", "Nolan", 2020, 11.0); // Invalid rating
-
         } catch (IllegalArgumentException e) {
             System.out.println("Error: " + e.getMessage());
         }
